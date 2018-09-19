e049b1e3c00970090b79d0f877d55cf2b8e5822a
Michael Griffin <mdgriffin064@gmail.com>
2018-09-19 14:30:42 +0100

    Formatting fix

--- app.js
+++ app.js
@@ -12,32 +12,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // http://localhost:5984/_utils/
 
-app.get('/', (req, res) => {
-    /*
-    let teamsDb = nano.use('teams');
-
-    // alice.insert({ happy: true }, 'rabbit')
-
-    teamsDb.get('').then((body) => {
-        res.send(data)
-    }).catch(err => {
-        res.send(err)
-    })
-    */
-
-    /*
-    nano.db.create('teams').then((data) => {
-        // success - response is in 'data'
-        res.send(data)
-      }).catch((err) => {
-        // failure - error information is in 'err'
-        res.send(err)
-      })
-    */
-
-    res.send('Hello World')
-})
-
 app.get('/teams', (req, res) => {
     let teamsDb = nano.use('teams');
 
@@ -45,14 +19,15 @@ app.get('/teams', (req, res) => {
         let teams = [];
         body.rows.forEach((doc) => teams.push(doc))
         res.json(teams);
-      });
+    });
 })
 
 app.post('/teams', (req, res) => {
     let teamsDb = nano.use('teams');
 
     // can add pass a key as the second param
-    teamsDb.insert(req.body)
+    teamsDb
+        .insert(req.body)
         .then(result => res.send(result))
         .catch(err => res.send(err))
 })
