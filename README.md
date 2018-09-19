364caf6f839824e6923adad0617dffdb95fec77d
Michael Griffin <mdgriffin064@gmail.com>
2018-09-19 21:30:30 +0100

    Added pug view engine

---
+++ README.md
@@ -0,0 +1,6 @@
+## CouchDB CRUD Application
+
+### To-Do
+
+- Add a view/templating library
+- Add Vue dependency and setup build chain
--- app.js
+++ app.js
@@ -2,28 +2,36 @@ const express = require('express')
 const app = express()
 const port = 3000
 const nano = require('nano')('http://localhost:5984')
+const pug = require('pug');
 
-var bodyParser = require('body-parser')
-var multer = require('multer')
-var upload = multer()
+const bodyParser = require('body-parser')
+const multer = require('multer')
+const upload = multer()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'pug')
+app.set('views', './app/views')
 
 // http://localhost:5984/_utils/
 
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+
+
 app.get('/teams', (req, res) => {
-    let teamsDb = nano.use('teams');
+    let teamsDb = nano.use('teams')
 
     teamsDb.list({include_docs: true}).then((body) => {
-        let teams = [];
+        let teams = []
         body.rows.forEach((doc) => teams.push(doc))
-        res.json(teams);
+        res.json(teams)
     });
 })
 
 app.post('/teams', (req, res) => {
-    let teamsDb = nano.use('teams');
+    let teamsDb = nano.use('teams')
 
     // can add pass a key as the second param
     teamsDb
@@ -33,18 +41,18 @@ app.post('/teams', (req, res) => {
 })
 
 app.delete('/teams/:teamId', (req, res) => {
-    let teamsDb = nano.use('teams');
+    let teamsDb = nano.use('teams')
 
     teamsDb.get(req.params.teamId).then((body) => {
         teamsDb
             .destroy(body._id, body._rev)
-            .then((resukt) => {
+            .then((result) => {
                 res.send(result)
             })
             .catch(err => {
-                res.send(err);
+                res.send(err)
             })
       });
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Listening on port ${port}!`))
