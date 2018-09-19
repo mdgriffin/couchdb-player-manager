f85509becf4462fb617ff9cb852cfd4ec05b4ae4
Michael Griffin <mdgriffin064@gmail.com>
2018-09-19 13:54:21 +0100

    Added connection to couchdb

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const nano = require('nano')('http://localhost:5984')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// http://localhost:5984/_utils/
+
+app.get('/', (req, res) => {
+    nano.db.create('teams').then((data) => {
+        // success - response is in 'data'
+        res.send(data)
+      }).catch((err) => {
+        // failure - error information is in 'err'
+        res.send(err)
+      })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
