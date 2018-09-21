c7a671e9de560eb9635090f981f13b2d7f47f7da
Michael Griffin <mdgriffin064@gmail.com>
2018-09-21 11:01:16 +0100

    Added basic teams listing

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 - Add Vue dependency and setup build chain
 - Add unit tests
     - Tests also needed for CouchDB map/reduce function, tested with sample data
+- Try and get setup script to populate couchdb with player stats
 
 
 ## Project setup
--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Teams</h1>
+    <div class="teamList" v-for="team in teams" :key="team._id">
+      <h2>{{team.name }}</h2>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  components: { },
+  data() {
+    return {
+      teams: []
+    }
   }
 }
 </script>
