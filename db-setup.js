8b165cb2230d532dd7ec1a16e0bcdee73c69994e
Michael Griffin <mdgriffin064@gmail.com>
2018-09-21 13:32:58 +0100

    Added script to populate database with sample data

---
+++ db-setup.js
@@ -0,0 +1,38 @@
+const csv = require("csvtojson");
+const nano = require("nano")("http://localhost:5984");
+const clubsDb = nano.use("clubs");
+
+const csvFilePath = "player_dataset.csv";
+
+csv()
+  .fromFile(csvFilePath)
+  .then(data => {
+    insertBulk(data);
+  });
+
+function insertBulk (data) {
+  let clubs = {};
+  let docs = []
+
+  data.forEach(player => {
+    let clubName = player["Club"]
+    let clubKey = clubName.replace(" ", "_").toLowerCase();
+    
+    if (clubs.hasOwnProperty(clubKey)) {
+      clubs[clubKey].players.push(player)
+    } else {
+      clubs[clubKey] = {
+        name: clubName,
+        players: [].concat(player)
+      }
+    }
+  })
+
+  Object.keys(clubs).forEach(key => {
+    docs.push(clubs[key]);
+  })
+
+  clubsDb.bulk({docs:docs}).then((body) => {
+    console.log(body);
+  });
+}
