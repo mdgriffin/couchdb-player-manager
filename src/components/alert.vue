fb79f297c3d4ffe26401910e4fcd32af7f0e4735
Michael Griffin <mdgriffin064@gmail.com>
2018-09-28 14:01:26 +0100

    Added new alert component and used it in the update view

---
+++ src/components/alert.vue
@@ -0,0 +1,25 @@
+<template>
+    <div v-if="show" :class="'alert ' + customClass">
+        <slot></slot>
+        <button @click="closeAlert">Close Alert</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'alert',
+    props: {
+        customClass: {
+            type: String,
+            default: ''
+        },
+        show: Boolean
+    },
+    methods: {
+        closeAlert () {
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
--- src/views/UpdatePlayerView.vue
+++ src/views/UpdatePlayerView.vue
@@ -3,6 +3,7 @@
         <loader :loading="playerLoading">
           <template slot="content" slot-scope="_">
             <h2>Update Player</h2>
+            <alert v-bind:show="showUpdatedAlert" @close="closeUpdatedAlert">Player Update Successfully</alert>
             <div>
                 <label>Name</label>
                 <input type="text" v-model="player.Name" />
@@ -44,17 +45,20 @@
 <script>
 import { getPlayerById, updatePlayer } from "../db/players-db";
 import Loader from "../components/loader.vue";
+import Alert from "../components/alert.vue";
 
 export default {
   name: "update-player-view",
    data() {
     return {
+      showUpdatedAlert: false,
       playerLoading: true,
       player: {}
     };
   },
   components: {
-    loader: Loader
+    loader: Loader,
+    alert: Alert
   },
   methods: {
     updatePlayer () {
@@ -63,10 +67,15 @@ export default {
       .then(res => {
         self.player._id = res.id
         self.player._rev = res.rev
+        self.showUpdatedAlert = true
       })
       .catch(err => {
         console.error(err)
       })
+    },
+    closeUpdatedAlert () {
+      console.log('Closing alert')
+      this.showUpdatedAlert = false
     }
   },
   created () {
