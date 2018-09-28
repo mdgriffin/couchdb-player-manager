9783751d8173d344d432dd12af5fc2e04c982895
Michael Griffin <mdgriffin064@gmail.com>
2018-09-28 14:10:32 +0100

    Added alert message to new player view

--- src/views/CreatePlayerView.vue
+++ src/views/CreatePlayerView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="createPlayer">
     <h2>Add Player</h2>
+    <alert v-bind:show="showAlert" @close="closeAlert">Player Created Successfully</alert>
     <div>
         <label>Name</label>
         <input type="text" v-model="name" />
@@ -39,11 +40,13 @@
 
 <script>
 import  {addPlayer} from '../db/players-db'
+import Alert from "../components/alert.vue";
 
 export default {
     name: 'create-players-view',
     data () {
         return {
+            showAlert: false,
             name: "",
             age: "",
             nationality: "",
@@ -54,8 +57,12 @@ export default {
             wage: ""
         }
     },
+    components: {
+        alert: Alert
+    },
     methods: {
         createPlayer () {
+            var self = this
             addPlayer({
                 'Name': this.name,
                 'Age': this.age,
@@ -66,10 +73,25 @@ export default {
                 'Value': this.value,
                 'Wage': this.wage
             }).then(res => {
-                console.log(res)
+                self.showAlert = true
+                self.resetForm()
+
             }).catch(err => {
                 console.log(err)
             })
+        },
+        closeAlert () {
+            this.showAlert = false
+        },
+        resetForm () {
+            this.name = ""
+            this.age = ""
+            this.nationality = ""
+            this.overall = ""
+            this.potential = ""
+            this.club = ""
+            this.value = ""
+            this.wage = ""
         }
     }
 }
