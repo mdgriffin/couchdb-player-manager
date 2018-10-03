f245d6b8cc77d6f9b6d35f405e373c5745f83b47
Michael Griffin <mdgriffin064@gmail.com>
2018-10-03 13:32:38 +0100

    Removed redundant code from the database setup script

--- db-setup.js
+++ db-setup.js
@@ -1,46 +1,12 @@
 const csv = require("csvtojson");
 const nano = require("nano")("http://localhost:5984");
-const clubsDb = nano.use("clubs");
-const playersDb = nano.use("players")
-
+const playersDb = nano.use("players");
 const csvFilePath = "player_dataset.csv";
 
 csv()
   .fromFile(csvFilePath)
   .then(data => {
-    //insertBulk(data);
-    insertPlayers(data)
+    playersDb.bulk({ docs: data }).then(result => {
+      console.log(result);
+    });
   });
-
-function insertBulk (data) {
-  let clubs = {};
-  let docs = []
-
-  data.forEach(player => {
-    let clubName = player["Club"]
-    let clubKey = clubName.replace(" ", "_").toLowerCase();
-    
-    if (clubs.hasOwnProperty(clubKey)) {
-      clubs[clubKey].players.push(player)
-    } else {
-      clubs[clubKey] = {
-        name: clubName,
-        players: [].concat(player)
-      }
-    }
-  })
-
-  Object.keys(clubs).forEach(key => {
-    docs.push(clubs[key]);
-  })
-
-  clubsDb.bulk({docs: docs}).then((body) => {
-    console.log(body);
-  });
-}
-
-function insertPlayers (players) {
-  playersDb.bulk({docs: players}).then((body) => {
-    console.log(body);
-  });
-}
